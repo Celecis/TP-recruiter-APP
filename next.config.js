cf021daf5af95aa44007e71968019352896af1d3
Celecis <vanessa.jv.torres@gmail.com>
2022-01-19 04:17:29 +0000

    projecto estágio REACT+TS+NEXT+MONGODB+AXIOS

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: { domains: ["res.cloudinary.com"] },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
